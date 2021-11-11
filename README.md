bd9d35a6679bf7212bcf64606e680a66b4423ddd
danielcamejo <dcamejo1@ufl.edu>
2021-11-11 13:42:34 -0500

    Modified Readme and added cpp file for testing code

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # FindMyWay
 Git repository used for collaboration within the Find My Way project, a C++ program made for identifying the most cost efficient route to take from point A to point B.
-
-Testing pushing
---
+++ testing.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<map>
+#include<stack>
+#include<string>
+
+using namespace std;
+
+int main(){
+    cout << "\n\n" << endl;
+
+    cout << "Hello world" << endl;
+
+    cout << "\n\n" << endl;
+    return 0;
+}
